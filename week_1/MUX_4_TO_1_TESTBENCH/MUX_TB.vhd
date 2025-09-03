eabaef6613d03cc0dde9c108e2763cbe9fdda2bc
GOCL01 <95654042+GOCL01@users.noreply.github.com>
2025-09-03 12:29:02 +0200

    Update MUX_TB.vhd

--- week_1/MUX_4_TO_1_TESTBENCH/MUX_TB.vhd
+++ week_1/MUX_4_TO_1_TESTBENCH/MUX_TB.vhd
@@ -1,53 +1,72 @@
 library IEEE;
-use IEEE.Std_logic_1164.all;
+use IEEE.std_logic_1164.all;
 
-entity MUX_TB is
-end entity MUX_TB;
+entity tb_MUX4 is
+end entity;
 
-architecture BENCH of MUX_TB is
+architecture sim of tb_MUX4 is
 
-  signal IN0       : Std_logic := '0';
-  signal IN1       : Std_logic := '0';
-  signal IN2       : Std_logic := '0';
-  signal IN3       : Std_logic := '0';
-  signal SEL0      : Std_logic := '0';
-  signal SEL1      : Std_logic := '0';
-  
-  signal MUX_OUT   : Std_logic;
+
+
+    -- Signals for DUT
+    signal DATA_IN_01 : std_logic := '0';
+    signal DATA_IN_02 : std_logic := '0';
+    signal DATA_IN_03 : std_logic := '0';
+    signal DATA_IN_04 : std_logic := '0';
+    signal S1, S2    : std_logic := '0';
+    signal DATA_OUT  : std_logic;
 
 begin
-  
-  IN0 <= not IN0 after 100 NS;
-  IN1 <= not IN1 after 50  NS;
-  IN2 <= not IN2 after 25  NS;
-  IN3 <= not IN3 after 12  NS;
+    -- DUT Instantiation
+    uut: entity work.MUX4
+        port map (
+            DATA_IN_01 => DATA_IN_01,
+            DATA_IN_02 => DATA_IN_02,
+            DATA_IN_03 => DATA_IN_03,
+            DATA_IN_04 => DATA_IN_04,
+            S1 => S1,
+            S2 => S2,
+            DATA_OUT => DATA_OUT
+        );
 
-  process
-  begin
-    SEL0 <= '0'; SEL1 <= '0'; wait for  1 US;
-	-- output of mux should be IN0
-	
-	SEL0 <= '1'; SEL1 <= '0'; wait for  1 US;
-	-- output of mux should be IN1
-	
-	SEL0 <= '0'; SEL1 <= '1'; wait for  1 US;
-	-- output of mux should be IN2
-	
-	SEL0 <= '1'; SEL1 <= '1'; wait for  1 US;
-    -- output of mux should be IN3
-	
-	wait;
-  end process;
+    -- Stimulus Process
+    stim_proc: process
+    begin
+        -- Test 1: DATA_IN_01 selected
+        DATA_IN_01 <= '1'; DATA_IN_02 <= '0'; DATA_IN_03 <= '0'; DATA_IN_04 <= '0';
+        S1 <= '0'; S2 <= '0';
+        wait for 10 ns;
 
-  M: entity work.MULTIPLEXER
-  port map (
-    IN0  => IN0,
-    IN1  => IN1,
-    IN2  => IN2,
-    IN3  => IN3,
-    SEL0 => SEL0,
-    SEL1 => SEL1,
-    MUX_OUT => MUX_OUT
-	);
+        -- Test 2: DATA_IN_02 selected
+        DATA_IN_01 <= '0'; DATA_IN_02 <= '1';
+        S1 <= '0'; S2 <= '1';
+        wait for 10 ns;
 
-end architecture BENCH;
+        -- Test 3: DATA_IN_03 selected
+        DATA_IN_02 <= '0'; DATA_IN_03 <= '1';
+        S1 <= '1'; S2 <= '0';
+        wait for 10 ns;
+
+        -- Test 4: DATA_IN_04 selected
+        DATA_IN_03 <= '0'; DATA_IN_04 <= '1';
+        S1 <= '1'; S2 <= '1';
+        wait for 10 ns;
+
+        -- Additional tests with mixed values
+        DATA_IN_01 <= '1'; DATA_IN_02 <= '1'; DATA_IN_03 <= '0'; DATA_IN_04 <= '0';
+        S1 <= '0'; S2 <= '0';  
+        wait for 10 ns;
+
+        S1 <= '0'; S2 <= '1'; 
+        wait for 10 ns;
+
+        S1 <= '1'; S2 <= '0';  
+        wait for 10 ns;
+
+        S1 <= '1'; S2 <= '1';  
+        wait for 10 ns;
+
+        wait; -- end simulation
+    end process;
+
+end architecture sim;
