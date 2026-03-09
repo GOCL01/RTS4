b664c1657a6e210ad3f52bb87681e51b594b1444
GOCL01 <95654042+GOCL01@users.noreply.github.com>
2026-03-09 13:59:29 +0100

    Update D_FF_Example.vhd

--- week2/excersise_2/D_FF_Example.vhd
+++ week2/excersise_2/D_FF_Example.vhd
@@ -3,9 +3,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity D_FF_Example is
 port (
-      D : in  std_logic;   -- input 
-      CLK : in  std_logic; -- clock signal
-      Q: out std_logic);   -- output
+
+	-- student adds inputs and outputs
+      );   
 end entity D_FF_Example;
 
 architecture behavioral of D_FF_Example is
