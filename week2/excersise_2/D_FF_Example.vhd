181a67205d80226663aa2fb04d0c66b32c1fb944
GOCL01 <95654042+GOCL01@users.noreply.github.com>
2026-03-09 13:58:29 +0100

    Create D_FF_Example.vhd

---
+++ week2/excersise_2/D_FF_Example.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity D_FF_Example is
+port (
+      D : in  std_logic;   -- input 
+      CLK : in  std_logic; -- clock signal
+      Q: out std_logic);   -- output
+end entity D_FF_Example;
+
+architecture behavioral of D_FF_Example is
+begin 
+
+  -- student adds D-Flip-flop here
+
+	
+end architecture behavioral;
