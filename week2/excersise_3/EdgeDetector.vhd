1f838d2c4f9653cd9dbb5628595df40c744d3fef
GOCL01 <95654042+GOCL01@users.noreply.github.com>
2026-03-09 14:29:19 +0100

    Create EdgeDetector.vhd

---
+++ week2/excersise_3/EdgeDetector.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity EdgeDetector is
+port (
+      -- TODO : add inputs/outputs
+      );
+end entity EdgeDetector;
+
+architecture behavior of EdgeDetector is
+
+-- TODO : add necessary signals
+
+begin 
+  
+   -- TODO: Add combinatorial logic if necessary
+  
+  process(CLK) is
+  begin
+  -- TODO: Add sequential logic
+  
+  end process;
+end architecture behavior;
