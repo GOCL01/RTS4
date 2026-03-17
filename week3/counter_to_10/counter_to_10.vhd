b716a2d7066b2b9258a344b0b05c2eae315a134d
GOCL01 <95654042+GOCL01@users.noreply.github.com>
2026-03-17 11:01:31 +0100

    Create counter_to_10.vhd

---
+++ week3/counter_to_10/counter_to_10.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity counter_to_10 is
+    Port (
+     -- Define ports
+    );
+end counter_to_10;
+
+architecture Behavioral of counter_to_10 is
+   -- define a signal that stores the value of your counter
+begin
+
+--  define counter
+
+end Behavioral;
